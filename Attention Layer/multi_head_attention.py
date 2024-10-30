fix (MultiHeadAttention) : Missunderstanding in MultiHead, fixed Projection > reshaping for mh > matmul > softmax > matmul > concat and linear pj again

Attention Layer/multi_head_attention.py
@@ -6,35 +6,45 @@ from single_head_attention import SingleHeadAttentionLayer
 class MultiHeadAttentionLayer(nn.Module):
     def __init__(self, num_heads: int, d_model: int):
         super(MultiHeadAttentionLayer, self).__init__()
+        assert d_model % num_heads == 0
+
         self.num_heads = num_heads
         self.d_model = d_model
         self.head_dim = d_model // num_heads
 
+        # Linear projections
         self.linear_q = nn.Linear(d_model, d_model)
         self.linear_k = nn.Linear(d_model, d_model)
         self.linear_v = nn.Linear(d_model, d_model)
+
+        # Final linear for output
         self.linear_o = nn.Linear(d_model, d_model)
 
-        self.attention_layer = SingleHeadAttentionLayer(self.head_dim)
-
     def forward(self, q, k, v, mask=None):
+        # Start with linear projection
         # Shape: (batch_size, seq_length, d_model)
         q = self.linear_q(q)
         k = self.linear_k(k)
         v = self.linear_v(v)
 
+        # Reshaping
         # Shape: (batch_size, num_heads, seq_length, head_dim)
         q = q.view(q.size(0), q.size(1), self.num_heads, self.head_dim).transpose(1, 2)
         k = k.view(k.size(0), k.size(1), self.num_heads, self.head_dim).transpose(1, 2)
         v = v.view(v.size(0), v.size(1), self.num_heads, self.head_dim).transpose(1, 2)
 
-        headi = []
-        for i in range(self.num_heads):
-            head_output = self.attention_layer(q[:, i], k[:, i], v[:, i], mask)
-            headi.append(head_output)
+        # Perform attention scores
+        x = (q @ k.transpose(-2, -1)) / (k.size(-1) ** 0.5)
+        if mask is not None:
+            x = x.masked_fill(mask == 0, float('-inf'))
 
-        headn = torch.cat(headi, dim=-1)  # Shape: (batch_size, seq_length, d_model)
-        return self.linear_o(headn)
+        # softmax to convert logit scores to probs
+        x = torch.softmax(x, dim=-1)
+
+        # perform output
+        x = x @ v
+        x = x.transpose(1, 2).contiguous().view(batch_size, -1, self.d_model)
+        return self.linear_o(x)
 
 if __name__ == "__main__":
     # Dummy data