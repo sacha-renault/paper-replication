325203175b5c869033b63ce58a34925f775a40d7
sacha-renault <sacha35340@gmail.com>
2024-10-31 10:18:56 +0100

    feat (Transformer): add positional encoding

--- Attention Layer/src/transformer.py
+++ Attention Layer/src/transformer.py
@@ -68,9 +68,12 @@ class Transformer(nn.Module):
                  ffn_hidden_dim: int = 2048,
                  n_encoder_block: int = 6,
                  n_decoder_block: int = 6,
-                 vocab_size: int = 512):
+                 vocab_size: int = 512,
+                 max_sequence_len: int = 100):
         super(Transformer, self).__init__() # forgot this previously
         self.d_model = d_model
+        self._max_len = max_sequence_len
+        self._set_positional_encoding()
 
         # encoder
         self.encoder_embedding = nn.Embedding(vocab_size, d_model)
@@ -85,10 +88,34 @@ class Transformer(nn.Module):
         # output
         self.linear_output = nn.Linear(d_model, vocab_size)
 
+    def _set_positional_encoding(self) -> None:
+        # positional encoding
+        positions = torch.arange(0, self._max_len).unsqueeze(1).repeat(1, self.d_model)
+        dimensions = torch.arange(0, self.d_model).unsqueeze(0)
+        angle_rates = 1 / (10_000 ** (dimensions // 2 * 2 / self.d_model))
+        positional_encoding = torch.zeros((self._max_len, self.d_model))
+        positional_encoding[:, 0::2] = torch.sin(positions * angle_rates[:, 0::2])
+        positional_encoding[:, 1::2] = torch.cos(positions * angle_rates[:, 1::2])
+        self.positional_encoding = positional_encoding.unsqueeze(0)
+
+    @property
+    def max_sequence_length(self) -> int:
+        return self._max_len
+
+    @max_sequence_length.setter
+    def max_sequence_length(self, new_value: int) -> None:
+        self._max_len = new_value
+        self._set_positional_encoding()
+
     def forward(self, inputs: Tensor, outputs: Tensor, encoder_mask = None, decoder_mask = None) -> Tensor:
+        # ensure positional encoding isn't smaller than max len
+        assert inputs.size(1) <= self._max_len and outputs.size(1) <= self._max_len (
+            "Input size is greater than max sequence length"
+            "Considere truncating or increase max seq size")
+
         # encoder part
         x = self.encoder_embedding(inputs) * (self.d_model ** 0.5)
-        # TODO need to add positional encoding
+        x += self.positional_encoding[:, :inputs.size(1), :]
         for block in self.encoder:
             x = block(x, encoder_mask)
 
@@ -98,7 +125,7 @@ class Transformer(nn.Module):
         # decoder part, it takes the output of encoder as query
         # for the cross attention part
         x = self.decoder_embedding(outputs) * (self.d_model ** 0.5)
-        # TODO need to add positional encoding
+        x += self.positional_encoding[:, :outputs.size(1), :]
         for block in self.decoder:
             x = block(x, encoder_output, decoder_mask)
 
